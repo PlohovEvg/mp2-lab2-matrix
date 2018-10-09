449d18a6eea5c5f5685fecfb536dfd648421f3e0
PlohovEvg <plohov99@yandex.ru>
2018-10-10 00:40:39 +0300

    commented some unused tests

--- test/test_tvector.cpp
+++ test/test_tvector.cpp
@@ -29,15 +29,15 @@ TEST(TVector, can_create_copied_vector)
   ASSERT_NO_THROW(TVector<int> v1(v));
 }
 
-TEST(TVector, copied_vector_is_equal_to_source_one)
-{
-  ADD_FAILURE();
-}
-
-TEST(TVector, copied_vector_has_its_own_memory)
-{
-  ADD_FAILURE();
-}
+//TEST(TVector, copied_vector_is_equal_to_source_one)
+//{
+//  ADD_FAILURE();
+//}
+//
+//TEST(TVector, copied_vector_has_its_own_memory)
+//{
+//  ADD_FAILURE();
+//}
 
 TEST(TVector, can_get_size)
 {
@@ -61,93 +61,93 @@ TEST(TVector, can_set_and_get_element)
   EXPECT_EQ(4, v[0]);
 }
 
-TEST(TVector, throws_when_set_element_with_negative_index)
-{
-  ADD_FAILURE();
-}
-
-TEST(TVector, throws_when_set_element_with_too_large_index)
-{
-  ADD_FAILURE();
-}
-
-TEST(TVector, can_assign_vector_to_itself)
-{
-  ADD_FAILURE();
-}
-
-TEST(TVector, can_assign_vectors_of_equal_size)
-{
-  ADD_FAILURE();
-}
-
-TEST(TVector, assign_operator_change_vector_size)
-{
-  ADD_FAILURE();
-}
-
-TEST(TVector, can_assign_vectors_of_different_size)
-{
-  ADD_FAILURE();
-}
-
-TEST(TVector, compare_equal_vectors_return_true)
-{
-  ADD_FAILURE();
-}
-
-TEST(TVector, compare_vector_with_itself_return_true)
-{
-  ADD_FAILURE();
-}
-
-TEST(TVector, vectors_with_different_size_are_not_equal)
-{
-  ADD_FAILURE();
-}
-
-TEST(TVector, can_add_scalar_to_vector)
-{
-  ADD_FAILURE();
-}
-
-TEST(TVector, can_subtract_scalar_from_vector)
-{
-  ADD_FAILURE();
-}
-
-TEST(TVector, can_multiply_scalar_by_vector)
-{
-  ADD_FAILURE();
-}
-
-TEST(TVector, can_add_vectors_with_equal_size)
-{
-  ADD_FAILURE();
-}
-
-TEST(TVector, cant_add_vectors_with_not_equal_size)
-{
-  ADD_FAILURE();
-}
-
-TEST(TVector, can_subtract_vectors_with_equal_size)
-{
-  ADD_FAILURE();
-}
-
-TEST(TVector, cant_subtract_vectors_with_not_equal_size)
-{
-  ADD_FAILURE();
-}
-
-TEST(TVector, can_multiply_vectors_with_equal_size)
-{
-  ADD_FAILURE();
-}
-
-TEST(TVector, cant_multiply_vectors_with_not_equal_size)
-{
-  ADD_FAILURE();
-}
+//TEST(TVector, throws_when_set_element_with_negative_index)
+//{
+//  ADD_FAILURE();
+//}
+//
+//TEST(TVector, throws_when_set_element_with_too_large_index)
+//{
+//  ADD_FAILURE();
+//}
+//
+//TEST(TVector, can_assign_vector_to_itself)
+//{
+//  ADD_FAILURE();
+//}
+//
+//TEST(TVector, can_assign_vectors_of_equal_size)
+//{
+//  ADD_FAILURE();
+//}
+//
+//TEST(TVector, assign_operator_change_vector_size)
+//{
+//  ADD_FAILURE();
+//}
+//
+//TEST(TVector, can_assign_vectors_of_different_size)
+//{
+//  ADD_FAILURE();
+//}
+//
+//TEST(TVector, compare_equal_vectors_return_true)
+//{
+//  ADD_FAILURE();
+//}
+//
+//TEST(TVector, compare_vector_with_itself_return_true)
+//{
+//  ADD_FAILURE();
+//}
+//
+//TEST(TVector, vectors_with_different_size_are_not_equal)
+//{
+//  ADD_FAILURE();
+//}
+//
+//TEST(TVector, can_add_scalar_to_vector)
+//{
+//  ADD_FAILURE();
+//}
+//
+//TEST(TVector, can_subtract_scalar_from_vector)
+//{
+//  ADD_FAILURE();
+//}
+//
+//TEST(TVector, can_multiply_scalar_by_vector)
+//{
+//  ADD_FAILURE();
+//}
+//
+//TEST(TVector, can_add_vectors_with_equal_size)
+//{
+//  ADD_FAILURE();
+//}
+//
+//TEST(TVector, cant_add_vectors_with_not_equal_size)
+//{
+//  ADD_FAILURE();
+//}
+//
+//TEST(TVector, can_subtract_vectors_with_equal_size)
+//{
+//  ADD_FAILURE();
+//}
+//
+//TEST(TVector, cant_subtract_vectors_with_not_equal_size)
+//{
+//  ADD_FAILURE();
+//}
+//
+//TEST(TVector, can_multiply_vectors_with_equal_size)
+//{
+//  ADD_FAILURE();
+//}
+//
+//TEST(TVector, cant_multiply_vectors_with_not_equal_size)
+//{
+//  ADD_FAILURE();
+//}
 
